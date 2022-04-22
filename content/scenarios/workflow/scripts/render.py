3359aec00abfdfa27b086e0594da0e53febc090a
Johannes Köster <johannes.koester@uni-due.de>
2022-04-22 12:11:59 +0200

    fix: always check scenarios, fixes in various scenarios

--- content/scenarios/workflow/scripts/render.py
+++ content/scenarios/workflow/scripts/render.py
@@ -6,13 +6,22 @@ from itertools import product
 meta = yaml.load(open(snakemake.input.meta, "r"), Loader=yaml.FullLoader)
 
 plot_prior = meta.get("plot-prior", False)
-print(plot_prior)
 prior_plots = {}
-if plot_prior:
-    for sample, contig in product(plot_prior["samples"], plot_prior["contigs"]):
-        cmd = ["varlociraptor", "plot", "variant-calling-prior", "--sample", str(sample), "--contig", str(contig), "--scenario", snakemake.input.scenario]
-        prior_plots[(sample, contig)] = sp.run(cmd, stdout=sp.PIPE, check=True, text=True).stdout
 
-print(prior_plots)
+if plot_prior:
+    samples = plot_prior["samples"]
+    contigs = plot_prior["contigs"]
+else:
+    with open(snakemake.input.scenario, "r") as infile:
+        scenario = yaml.load(infile, Loader=yaml.SafeLoader)
+        samples = [next(iter(scenario["samples"].keys()))]
+        contigs = ["1"]
+
+for sample, contig in product(samples, contigs):
+    cmd = ["varlociraptor", "plot", "variant-calling-prior", "--sample", str(sample), "--contig", str(contig), "--scenario", snakemake.input.scenario]
+    res = sp.run(cmd, stdout=sp.PIPE, check=True, text=True)
+    if plot_prior:
+        prior_plots[(sample, contig)] = res.stdout
+
 with open(snakemake.input.template, "r") as template, open(snakemake.input.scenario, "r") as scenario, open(snakemake.output[0], "w") as out:
     print(Template(template.read()).render(scenario=scenario.read(), prior_plots=prior_plots, name=meta["name"], desc=meta["desc"]), file=out)
